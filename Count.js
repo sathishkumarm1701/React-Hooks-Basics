a7f89e8e51b2030c02d1e5e712738b0550b607d5
sathishkumarm1701 <94674492+sathishkumarm1701@users.noreply.github.com>
2023-11-09 20:21:35 +0530

    Update Count.js

--- Count.js
+++ Count.js
@@ -1,16 +1,16 @@
- import React,{useState} from "react";
+import React,{useState} from "react";
 const Count = () => {
-    const[clicks,setClicks] = useState(0);
-    function handleButtonClicks() {
-        setClicks(clicks+1);
-        console.log(clicks+1)
-    }
-    return(
-        <div>
-            <span>You Clicked {clicks}clicks</span>
-            <br/>
-            <button onClick={handleButtonClicks}>Click Me!</button>
-        </div>
+ const[clicks,setClicks] = useState(0);
+ 
+  function handleButtonClicks() {
+     setClicks(clicks+1);
+     }
+  return(
+     <div>
+        <span>You Clicked {clicks}clicks</span>
+          <br/>
+         <button onClick={handleButtonClicks}>Click Me!</button>
+     </div>
     )
 }
 export default Count;
